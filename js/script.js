c9aefe679eb349c5a8c2c292a31b5aec1c88638d
unknown <kola.alda7@gmail.com>
2022-02-10 21:01:20 +0100

    Feb10

--- js/script.js
+++ js/script.js
@@ -27,16 +27,15 @@ agree1.onclick = function() {
   modal.style.display = "none";
 }
 // Validate Form
-
-
-
-
 function validation(){
 var name = document.getElementById("fname").value;
 var lastname = document.getElementById("lname").value;
 var email = document.getElementById("ename").value;
 var subject = document.getElementById("subject").value;
 var error_message = document.getElementById("error_message");
+var error_message1 = document.getElementById("error_message1");
+var error_message2 = document.getElementById("error_message2");
+var error_message3 = document.getElementById("error_message3");
 var text;
 if(name.length < 3){
   text = "! Please enter your name";
@@ -44,39 +43,67 @@ if(name.length < 3){
   error_message.style.display = "block";
   return false;
 }
+else{
+  document.getElementById("error_message").innerHTML = "";
+  document.getElementById("error_message").style.display = "none";
+}
 if(lastname.length < 4){
   text = "! Please enter your lastname.";
-  error_message.innerHTML = text;
-  error_message.style.display = "block";
+  error_message1.innerHTML = text;
+  error_message1.style.display = "block";
   return false;
+}
+else{
+  document.getElementById("error_message1").innerHTML = "";
+  document.getElementById("error_message1").style.display = "none";
+  
 }
 if(email.indexOf("@") == -1 || email.length < 5){
   text = "! Please enter your email.";
-  error_message.innerHTML = text;
-  error_message.style.display = "block";
+  error_message2.innerHTML = text;
+  error_message2.style.display = "block";
   return false;
 }
-if(subject.length < 100){
+else{
+  document.getElementById("error_message2").innerHTML = "";
+  document.getElementById("error_message2").style.display = "none";
+  
+}
+if(subject.length < 50){
   text = "! Please enter your subject.";
-  error_message.innerHTML = text;
-  error_message.style.display = "block";
+  error_message3.innerHTML = text;
+  error_message3.style.display = "block";
   return false;
+}
+else{
+  document.getElementById("error_message3").innerHTML = "";
+  document.getElementById("error_message3").style.display = "none";
+  
 }
 return true;
 }
 
-
-function required(){
+// document.getElementById('submit').disabled = true;
+// function required(){
   
-  if(document.getElementById('agree').checked) {
-    modal.style.display = "block";
-    return true; 
-   } 
-    else {
+//   if(document.getElementById('agree').checked) {
+//     modal.style.display = "block";
+//     return true; 
+//    } 
+//     else {
+//     alert('Please indicate that you have read and agree to the Terms and Conditions and Privacy Policy'); 
+//     return false; 
+//    }
+// }
+
+
+function checking(){
+  if(document.getElementById('agree').checked = true ){
+    document.getElementById('agree').checked;
+  }else {
     alert('Please indicate that you have read and agree to the Terms and Conditions and Privacy Policy'); 
     return false; 
    }
-   
 }
 
 
